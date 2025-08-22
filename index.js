3765aaaaf820dc2613a9d392c3724df962f1dfd9
Sachinyadav00 <139240460+Sachinyadav00@users.noreply.github.com>
2025-08-22 22:03:56 +0530

    added server for render 24/7

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
+const express = require("express");
+const app = express();
 const connectDB = require("./config/db");
 const dotenv = require("dotenv");
 dotenv.config();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 const initBot = require("./bot");
 
 (async () => {
   await connectDB();
   initBot();
 })();
+
+app.listen(3000, () => {
+  console.log("server listening on port 3000");
+});
